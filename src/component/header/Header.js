575c14a4a9abe4c97e9b08e4d2afdaacd6c0351a
Hanif <marbling1293@naver.com>
2021-05-14 11:26:38 +0900

    [ADD] Header Route

---
+++ src/component/header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => (
+    <div className="header">
+        <div className="notify">Notify</div>
+    </div>
+);
+
+export default Header;
