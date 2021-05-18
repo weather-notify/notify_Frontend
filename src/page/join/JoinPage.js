7eeb3dc44481228cc2f4aac3d7a796d3ea0584bd
Hanif <marbling1293@naver.com>
2021-05-18 16:22:07 +0900

    [ADD] submit container

--- src/page/join/JoinPage.js
+++ src/page/join/JoinPage.js
@@ -4,12 +4,14 @@ import * as S from './style';
 
 import HeaderContainer from 'container/headerContainer/HeaderContainer';
 import JoinContainer from 'container/joinContainer/JoinContainer';
+import SubmitContainer from 'container/commonContainer/SubmitContainer';
 
 export const JoinPage = () => {
     return (
         <S.Container>
             <HeaderContainer />
             <JoinContainer />
+            <SubmitContainer />
         </S.Container>
     )
 }
