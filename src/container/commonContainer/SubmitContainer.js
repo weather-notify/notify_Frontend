468556498dba226f47a4f534b3ed1c6d0b8df65d
Hanif <marbling1293@naver.com>
2021-05-18 16:00:45 +0900

    [ADD] submit container

---
+++ src/container/commonContainer/SubmitContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import SubmitButton from 'component/common/SubmitButton';
+
+const SubmitContainer = () => {
+    return (
+        <SubmitButton />
+    )
+}
+
+export default SubmitContainer;
