f14dda9740622da880c4670ef9a2f6586d9eb3db
Hanif <marbling1293@naver.com>
2021-05-26 11:03:34 +0900

    [DELETE] SubmitContainer

--- src/page/login/LoginPage.js
+++ src/page/login/LoginPage.js
@@ -4,14 +4,12 @@ import * as S from './style';
 
 import HeaderContainer from 'container/headerContainer/HeaderContainer';
 import LoginContainer from 'container/loginContainer/LoginContainer';
-import SubmitContainer from 'container/commonContainer/SubmitContainer';
 
 const LoginPage = () => {
     return (
         <S.Container>
             <HeaderContainer />
             <LoginContainer />
-            <SubmitContainer />
         </S.Container>
     )
 }
