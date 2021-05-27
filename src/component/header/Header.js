0d707e9710b357b9310dc5290f17561029183494
Hanif <marbling1293@naver.com>
2021-05-27 21:44:05 +0900

    [FIX] localStorage.getItem name

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -5,7 +5,7 @@ import { useHistory } from 'react-router';
 import * as S from './style';
 import * as I from 'asset/index';
 
-const token = localStorage.getItem("token");
+const token = localStorage.getItem("accessToken");
 
 
 const Header = () => {
